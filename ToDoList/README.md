13364e8cd5e49d4e883c7a3c87304ce591ef75d9
cheshirexlxl <cheshire.xlxl@gmail.com>
2026-02-23 18:00:20 +0900

    2월 23일 React TodoList 프로젝트

---
+++ ToDoList/README.md
@@ -0,0 +1,17 @@
+# 리액트 프로젝트 생성
+## To Do List 프로젝트
+
+1. create-vite todo-app --template react
+
+2. Component 구성
+ - components
+   - Container.jsx
+   - Header.jsx
+   - Input.jsx
+   - List.jsx
+   - Card.jsx
+   - Footer.jsx
+
+2. App
+ - App.css
+ - App.jsx 
---
+++ ToDoList/todo/src/main/java/com/aloha/todo/service/TodoServiceImpl.java
@@ -0,0 +1,87 @@
+package com.aloha.todo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.aloha.todo.domain.Todos;
+import com.aloha.todo.mapper.TodoMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TodoServiceImpl implements TodoService {
+
+    private final TodoMapper todoMapper;
+
+    @Override
+    public List<Todos> list() {
+        return todoMapper.list();
+    }
+
+    @Override
+    public PageInfo<Todos> list(int page, int size) {
+        PageHelper.startPage(page, size);
+        List<Todos> list = todoMapper.list();
+        PageInfo<Todos> pageInfo = new PageInfo<>(list);
+        pageInfo.getList().sort((t1, t2) -> {
+            int statusCompare = t1.getStatus().compareTo(t2.getStatus());
+            if (statusCompare != 0) {
+                return statusCompare;
+            }           
+            return t1.getSeq().compareTo(t2.getSeq());
+        });
+        return pageInfo;
+    }
+
+    @Override
+    public Todos select(Long no) {
+        return todoMapper.select(no);
+    }
+
+    @Override
+    public Todos selectById(String id) {
+        return todoMapper.selectById(id);
+    }
+
+    @Override
+    public boolean insert(Todos entity) {
+        return todoMapper.insert(entity) > 0;
+    }
+
+    @Override
+    public boolean update(Todos entity) {
+        return todoMapper.update(entity) > 0;
+    }
+
+    @Override
+    public boolean updateById(Todos entity) {
+        return todoMapper.updateById(entity) > 0;
+    }
+
+    @Override
+    public boolean delete(Long no) {
+        return todoMapper.delete(no) > 0;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return todoMapper.deleteById(id) > 0;
+    }
+
+    @Override
+    public boolean completeAll() throws Exception {
+        return todoMapper.completeAll() > 0;
+    }
+
+    @Override
+    public boolean deleteAll() throws Exception {
+        return todoMapper.deleteAll() > 0;
+    }
+    
+}
